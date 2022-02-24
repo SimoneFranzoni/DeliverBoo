557f171633076727685d18476a9e6957aa2cd30c
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2022-02-24 16:21:31 +0100

    migrations e model creati

---
+++ database/migrations/2022_02_24_151450_create_plates_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plates', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('restaurant_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->boolean('is_available');
+            $table->decimal('price');
+            $table->text('ingrediants');
+            $table->string('category');
+            $table->string('cover')->nullable();
+            $table->string('cover_original_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plates');
+    }
+}
--- public/js/app.js
+++ public/js/app.js
@@ -49931,8 +49931,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\franc\Documents\Francesco\BOOLEAN\Esercizi\PORTFOLIO\progetto finale\DeliverBoo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\franc\Documents\Francesco\BOOLEAN\Esercizi\PORTFOLIO\progetto finale\DeliverBoo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/albertonicolaciufici/Desktop/Boolean/Progetto Finale/DeliverBoo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/albertonicolaciufici/Desktop/Boolean/Progetto Finale/DeliverBoo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
