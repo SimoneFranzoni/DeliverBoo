915aad44a881f8da01fe81ccc502741854f869dd
francescolucco <francescolucco@googlemail.com>
2022-02-25 13:32:05 +0100

    risoluzione problema tabella user

--- database/migrations/2022_02_24_151900_update_users_table.php
+++ database/migrations/2022_02_24_151900_update_users_table.php
@@ -14,8 +14,8 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('surname')->after('name');
-            $table->date('date_of_birth');
+            $table->string('surname')->after('name')->nullable()->default('');
+            $table->date('date_of_birth')->after('surname')->nullable();
         });
     }
 
