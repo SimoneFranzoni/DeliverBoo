495003a064de7373d0f0a4a5a24c272a17d309ff
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2022-02-28 11:02:43 +0100

    divisione front/back

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+ mix.js('resources/js/admin/app.js', 'public/js/admin')
+ .js('resources/js/guest/app.js', 'public/js/guest')
+ .sass('resources/sass/admin/style.scss', 'public/css/admin')
+ .sass('resources/sass/guest/style.scss', 'public/css/guest')
 
 .options({
     processCssUrls: false
