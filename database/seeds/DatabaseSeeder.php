1a87bc24d6d0628ecce2832592ebd64ab5e0b0e3
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2022-02-28 10:22:14 +0100

    fix db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PlatesTableSeeder::class);
         $this->call(UpdateRestaurantsTableSeeder::class);
         $this->call(UpdatPlatesTableSeeder::class);
+        $this->call(TypesTableSeeder::class);
     }
 }
