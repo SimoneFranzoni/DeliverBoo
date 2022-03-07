413b88fa0dbf468bf5f95c320ea842270ecbca6a
simopan93 <diegobuonanotte10@gmail.com>
2022-03-07 18:31:50 +0100

    Carrello commit

--- public/js/guest/app.js
+++ public/js/guest/app.js
@@ -2145,6 +2145,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: 'Restaurantdetail',
@@ -2156,7 +2157,8 @@ __webpack_require__.r(__webpack_exports__);
       apiUrl: 'http://127.0.0.1:8000/api/ristoranti/',
       activeRestaurant: {},
       plates: [],
-      cartItems: []
+      cartItems: [],
+      carrello: []
     };
   },
   mounted: function mounted() {
@@ -2447,28 +2449,45 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: 'PlateBox',
   props: {
-    plate: Object
+    plate: Object,
+    carrello: Array
   },
   mounted: function mounted() {// console.log('MOUNTED >>', localStorage);
   },
   data: function data() {
     return {
       cartItemCounter: 0,
-      items: [],
-      singItem: {
+      // items: [],
+      totalSameItems: [],
+      item: {
         name: '',
         price: null
-      }
+      },
+      carrello: []
     };
   },
   methods: {
     saveItem: function saveItem(plate) {
-      this.singItem.name = localStorage.setItem('name', plate.name);
-      this.singItem.price = localStorage.setItem('price', plate.price);
-      this.items.push(this.singItem);
-      localStorage.setItem('items', JSON.stringify(this.items));
-      this.$emit('cartArray', this.items);
-      console.log(localStorage.getItem('items'));
+      this.item.name = plate.name, this.item.price = plate.price;
+      this.totalSameItems.push(this.item); // console.log(this.totalSameItems);
+
+      var jsonStr = JSON.stringify(this.totalSameItems);
+      localStorage.setItem('Storage Totale del Piatto Cliccato', jsonStr);
+      console.log(localStorage);
+      this.$emit('cartArray', localStorage); // jsonStr.name = localStorage.setItem('this.cart.name',plate.name);
+      // jsonStr.price = localStorage.setItem('this.cart.price',plate.price);
+      // console.log(localStorage);
+      // localStorage.setItem('Carrello', jsonStr);
+      // let cartValue = localStorage.getItem('cart');
+      // let cartObj = JSON.parse(cartValue);
+      // console.log('Oggetto del Carrello', cartObj);
+      // return cartObj;
+      // this.singItem.name = localStorage.setItem('name', plate.name);
+      // this.singItem.price = localStorage.setItem('price', plate.price);
+      // this.items.push(this.singItem);
+      // localStorage.setItem('items', JSON.stringify(this.items))
+      // this.$emit('cartArray', this.items);
+      // console.log(localStorage.getItem('items'));
     }
   }
 });
@@ -39334,7 +39353,7 @@ var render = function () {
               _vm._l(_vm.activeRestaurant.plates, function (plate, index) {
                 return _c("PlateBox", {
                   key: "plate" + index,
-                  attrs: { plate: plate },
+                  attrs: { plate: plate, carrello: _vm.carrello },
                   on: { cartArray: _vm.cartArray },
                 })
               }),
@@ -39539,9 +39558,9 @@ var render = function () {
                 [
                   _c("span", [_vm._v("v")]),
                   _vm._v(
-                    "\n                        " +
+                    "\r\n                        " +
                       _vm._s(type.name) +
-                      "\n\n                    "
+                      "\r\n\r\n                    "
                   ),
                 ]
               )
@@ -39687,7 +39706,7 @@ var render = function () {
               },
               [_vm._v("+")]
             ),
-            _vm._v(" \n        \n        0\n\n        "),
+            _vm._v(" \r\n        \r\n        0\r\n\r\n        "),
             _c("span", { attrs: { id: "remove" } }, [_vm._v("-")]),
           ]),
         ]),
@@ -55622,7 +55641,7 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/albertonicolaciufici/Desktop/Boolean/Progetto Finale/DeliverBoo/resources/js/guest/app.js */"./resources/js/guest/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Simone\Documents\Boolean\Progetto Finale\DeliverBoo\resources\js\guest\app.js */"./resources/js/guest/app.js");
 
 
 /***/ })
