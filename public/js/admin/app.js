d3942e54944950a8a72a9d896632e262e4329894
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2022-03-08 11:55:51 +0100

    ok

--- public/js/admin/app.js
+++ public/js/admin/app.js
@@ -37351,15 +37351,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-__webpack_require__(/*! C:\Users\franc\Documents\Francesco\BOOLEAN\Esercizi\PORTFOLIO\progetto finale\DeliverBoo\DeliverBoo\resources\js\admin\app.js */"./resources/js/admin/app.js");
-__webpack_require__(/*! C:\Users\franc\Documents\Francesco\BOOLEAN\Esercizi\PORTFOLIO\progetto finale\DeliverBoo\DeliverBoo\resources\sass\admin\style.scss */"./resources/sass/admin/style.scss");
-module.exports = __webpack_require__(/*! C:\Users\franc\Documents\Francesco\BOOLEAN\Esercizi\PORTFOLIO\progetto finale\DeliverBoo\DeliverBoo\resources\sass\guest\style.scss */"./resources/sass/guest/style.scss");
-=======
-__webpack_require__(/*! C:\Users\franz_u4goo5x\OneDrive\Desktop\DeliverBoo\resources\js\admin\app.js */"./resources/js/admin/app.js");
-__webpack_require__(/*! C:\Users\franz_u4goo5x\OneDrive\Desktop\DeliverBoo\resources\sass\admin\style.scss */"./resources/sass/admin/style.scss");
-module.exports = __webpack_require__(/*! C:\Users\franz_u4goo5x\OneDrive\Desktop\DeliverBoo\resources\sass\guest\style.scss */"./resources/sass/guest/style.scss");
->>>>>>> 7094bdfbbd1c22ac3eab72b5d3e541cf4a8e56e2
+__webpack_require__(/*! /Users/albertonicolaciufici/Desktop/Boolean/Progetto Finale/DeliverBoo/resources/js/admin/app.js */"./resources/js/admin/app.js");
+__webpack_require__(/*! /Users/albertonicolaciufici/Desktop/Boolean/Progetto Finale/DeliverBoo/resources/sass/admin/style.scss */"./resources/sass/admin/style.scss");
+module.exports = __webpack_require__(/*! /Users/albertonicolaciufici/Desktop/Boolean/Progetto Finale/DeliverBoo/resources/sass/guest/style.scss */"./resources/sass/guest/style.scss");
 
 
 /***/ })
